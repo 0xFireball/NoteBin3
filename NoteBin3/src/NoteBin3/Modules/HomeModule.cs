e568d4bc9aa14c0167b861cf18fddc73b4dafe07
0xFireball <0xFireball@outlook.com>
2016-10-05 19:28:08 -0700

    add homepage renderer

---
+++ NoteBin3/src/NoteBin3/Modules/HomeModule.cs
@@ -0,0 +1,15 @@
+﻿using Nancy;
+
+namespace NoteBin3.Modules
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get("/", args =>
+            {
+                return View["Home"];
+            });
+        }
+    }
+}
---
+++ NoteBin3/src/NoteBin3/NoteBinBootstrapper.cs
@@ -0,0 +1,28 @@
+﻿using Nancy;
+using Nancy.Bootstrapper;
+using Nancy.Conventions;
+using Nancy.Session;
+using Nancy.TinyIoc;
+
+namespace NoteBin3
+{
+    public class NoteBinBootstrapper : DefaultNancyBootstrapper
+    {
+        protected override void ConfigureConventions(NancyConventions nancyConventions)
+        {
+            base.ConfigureConventions(nancyConventions);
+            nancyConventions.StaticContentsConventions.Add(
+                StaticContentConventionBuilder.AddDirectory("wwwroot/assets", "assets/")
+            );
+            nancyConventions.StaticContentsConventions.Add(
+                StaticContentConventionBuilder.AddDirectory("wwwroot/static", "static/")
+            );
+        }
+
+        protected override void ApplicationStartup(TinyIoCContainer container, IPipelines pipelines)
+        {
+            base.ApplicationStartup(container, pipelines);
+            CookieBasedSessions.Enable(pipelines);
+        }
+    }
+}
