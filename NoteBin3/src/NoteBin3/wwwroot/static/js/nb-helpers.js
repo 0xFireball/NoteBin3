644fbdba25dfcb96108f7bb2c354802ba79d3c9a
0xFireball <0xFireball@outlook.com>
2016-10-07 14:44:30 -0700

    show error toastes and messages

---
+++ NoteBin3/src/NoteBin3/wwwroot/static/js/nb-helpers.js
@@ -0,0 +1,14 @@
+﻿/* jshint esversion: 6 */
+/*
+    Copyright 2016 0xFireball. All Rights Reserved.
+
+    https://github.com/0xFireball
+    https://0xfireball.me
+
+    This file is part of NoteBin3
+*/
+
+function showMessage(text, duration) {
+    duration = duration || 2400;
+    Materialize.toast(text, duration);
+}
