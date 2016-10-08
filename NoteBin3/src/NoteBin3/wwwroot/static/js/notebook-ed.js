ddca877dec2d34803b5d431ba55ff1b544766e2b
0xFireball <0xFireball@outlook.com>
2016-10-07 23:41:40 -0700

    add preview option to editing notebook

---
+++ NoteBin3/src/NoteBin3/wwwroot/static/js/notebook-ed.js
@@ -0,0 +1,71 @@
+/* jshint esversion: 6 */
+/*
+    Copyright 2016 0xFireball. All Rights Reserved.
+
+    https://github.com/0xFireball
+    https://0xfireball.me
+
+    This file is part of NoteBin3
+*/
+
+let codeMirrorOpts = {
+    mode: "gfm",
+    matchBrackets: true,
+    theme: "3024-day",
+    lineNumbers: true,
+    scrollbarStyle: "simple",
+    extraKeys: {
+        "Alt-F": "findPersistent"
+    },
+    viewportMargin: Infinity
+};
+
+let previewEnabled = $("#show-preview-cb").prop("checked");
+
+// Initialize CodeMirror editor
+let mdEditor = CodeMirror.fromTextArea($("#md-editor")[0], codeMirrorOpts);
+
+
+mdEditor.on("change", function(cm, change) {
+    //Editor modified event
+    localStorage.setItem("savedFile", mdEditor.getValue());
+    if (previewEnabled) {
+        updatePreview(mdEditor.getValue());
+    }
+    //send save request to server 
+});
+
+function restoreSavedSession() {
+    let savedFileCont = localStorage.getItem("savedFile");
+    if (savedFileCont) {
+        mdEditor.setValue(savedFileCont);
+    }
+}
+
+function updatePreview(inputMarkdown) {
+    $("#rendered-md").html(marked(inputMarkdown, {
+        sanitize: true
+    }));
+}
+
+function bootstrapEditor() {
+    //attempt to recover data
+    restoreSavedSession();
+}
+
+bootstrapEditor();
+
+$("#show-preview-cb").on("change", () => {
+    previewEnabled = $("#show-preview-cb").prop("checked");
+    if (previewEnabled) {
+        $("#preview-container").show();
+        $("#editor-container")
+            .removeClass("l12")
+            .addClass("l6");
+    } else {
+        $("#preview-container").hide();
+        $("#editor-container")
+            .removeClass("l6")
+            .addClass("l12");
+    }
+});
