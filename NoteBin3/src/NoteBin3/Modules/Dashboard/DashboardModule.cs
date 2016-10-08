b5b7e1f6d530eee01d19eafdca6f2f240809b7c6
0xFireball <0xFireball@outlook.com>
2016-10-07 19:11:53 -0700

    add footers and dashboard

---
+++ NoteBin3/src/NoteBin3/Modules/Dashboard/DashboardModule.cs
@@ -0,0 +1,17 @@
+﻿using Nancy;
+using Nancy.Security;
+
+namespace NoteBin3.Modules.Dashboard
+{
+    public class DashboardModule : NancyModule
+    {
+        public DashboardModule()
+        {
+            this.RequiresAuthentication();
+            Get("/dashboard", args =>
+            {
+                return View["App/Dashboard"];
+            });
+        }
+    }
+}
